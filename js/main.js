3e46b1514f6237e4b90ed3f93032db2a4c64e2a5
andresar784 <32715078+andresar784@users.noreply.github.com>
2021-09-18 21:37:59 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,14 +17,18 @@ const list = document.getElementById("weeklist");
 
 for (let i=0; i<links.length; i++)
     {
-        let listitems = document.createElement("li");
+      let listitems = document.createElement("li");
         let anchor = document.createElement("a");
-        //let label = document.createElement("label");
-        //list.setAttribute("label", links[i].label);
+        let img = document.createElement("img");
+        
+
+        
         anchor.setAttribute("href", links[i].url);
-      
+        img.setAttribute("src", links[i].image);
+        img.setAttribute("class", "pressed");
+        
         listitems.appendChild(anchor);
-       
+        anchor.appendChild(img);
         list.appendChild(listitems);
         
         
