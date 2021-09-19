b73a74c80e7ab7e94cbfd8893f265e14b1a5889b
andresar784 <32715078+andresar784@users.noreply.github.com>
2021-09-18 21:01:16 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ const links = [
   {
     label: "Week01",
     url: "week01/index.html"
-  }
+  },
   {
     label: "Week02",
     url: "week02/index.html"
-  }
+  },
 ]
 
 const list = document.getElementById("weeklist");
