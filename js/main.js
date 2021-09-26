afeef42e269eccfa9cb6e88badd7f49d87e91200
andresar784 <32715078+andresar784@users.noreply.github.com>
2021-09-25 22:44:56 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -26,22 +26,19 @@ for (let i=0; i<links.length; i++){
         list.appendChild(listitems);
 }
 
-
-//document.getElementById("myBtn").addEventListener("click", displayDate);
-
-//function displayDate() {
-  //document.getElementById("timetest").innerHTMl = Date();
-//}
-
-let x = document.getElementById("myBtn");
-x.addEventListener("click", displayDate);
-x.addEventListener("click", changeHTML);
-x.addEventListener("mouseover", overButton);
-x.addEventListener("mouseout", outButton);
+document.getElementById("myBtn").addEventListener("click", displayDate);
 
 function displayDate() {
   document.getElementById("timetest").innerHTMl = Date();
 }
+
+let x = document.getElementById("myBtn");
+
+x.addEventListener("click", changeHTML);
+x.addEventListener("mouseover", overButton);
+x.addEventListener("mouseout", outButton);
+
+
 
 function changeHTML(){
   document.getElementById("myBtn").innerHTML = 'Clicked!';
