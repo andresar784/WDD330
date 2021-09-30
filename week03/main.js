5c68bf4797cb478e4f310828a3dbbe0e4e374512
andresar784 <32715078+andresar784@users.noreply.github.com>
2021-09-30 11:54:16 -0300

    Update main.js

--- week03/main.js
+++ week03/main.js
@@ -1,1 +1,20 @@
+const d = new Date();
 
+let x = document.getElementById("myBtn");
+x.addEventListener("click", displayDate);
+x.addEventListener("click", changeHTML);
+x.addEventListener("mouseover", overButton);
+x.addEventListener("mouseout", outButton);
+
+function displayDate() {
+  document.getElementById("timetest").innerHTML = d.toDateString();
+}
+function changeHTML(){
+ document.getElementById("myBtn").innerHTML = 'Clicked!';
+}
+function overButton(){
+  document.getElementById("myBtn").innerHTML = 'You are over the button!';
+}
+function outButton(){
+  document.getElementById("myBtn").innerHTML = 'Try it';
+}
