710ad3f6e0cefa668b8abcaf9a6cec3294e095bf
Andres Rodriguez <rod16015@byui.edu>
2021-09-30 18:27:37 -0300

    minor changes to main index.html

--- js/main.js
+++ js/main.js
@@ -30,23 +30,3 @@ for (let i=0; i<links.length; i++){
         list.appendChild(listitems);
 }
 
-const d = new Date();
-
-let x = document.getElementById("myBtn");
-x.addEventListener("click", displayDate);
-x.addEventListener("click", changeHTML);
-x.addEventListener("mouseover", overButton);
-x.addEventListener("mouseout", outButton);
-
-function displayDate() {
-  document.getElementById("timetest").innerHTML = d.toDateString();
-}
-function changeHTML(){
- document.getElementById("myBtn").innerHTML = 'Clicked!';
-}
-function overButton(){
-  document.getElementById("myBtn").innerHTML = 'You are over the button!';
-}
-function outButton(){
-  document.getElementById("myBtn").innerHTML = 'Try it';
-}
