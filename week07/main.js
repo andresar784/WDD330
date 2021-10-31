b7058d8ab16588ae80f78203280aabc7b782b1e4
Andres Rodriguez <rod16015@byui.edu>
2021-10-31 01:11:12 -0300

    corrected minor oissues

--- week07/main.js
+++ week07/main.js
@@ -2,7 +2,7 @@ const textButton = document.getElementById('number');
 const apiButton = document.getElementById('chuck');
 const outputDiv = document.getElementById('output');
 
-const textURL = 'http://numbersapi.com/random';
+const textURL = 'https://catfact.ninja/fact';
 const apiURL = 'https://api.chucknorris.io/jokes/random';
 
 
