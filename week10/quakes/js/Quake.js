21da04279fd70b296cd5fbad2c6d7bc4c6e73fe5
Andres Rodriguez <rod16015@byui.edu>
2021-11-20 10:41:31 -0300

    CORRECTED MINOR

--- week10/quakes/js/Quake.js
+++ week10/quakes/js/Quake.js
@@ -7,7 +7,7 @@ export default class Quake {
     // store the last batch of retrieved quakes in the model.  I don't always do this...in this case the api doesn't have an endpoint to request one quake.
     this._quakes = [];
   }
-  async getEarthQuakesByRadius(position, radius = 5000) {
+  async getEarthQuakesByRadius(position, radius = 5555555) {
     // use the getJSON function and the position provided to build out the correct URL to get the data we need.  Store it into this._quakes, then return it
     const query =
       this.baseUrl +
@@ -18,10 +18,10 @@ export default class Quake {
   }
   getQuakeById(id) {
     // filter this._quakes for the record identified by id and return it
-   // this._quakes = await getJSON(
-      // this.baseUrl +
-      // `&starttime=2019-01-01&endtime=2019-03-02&latitude=${position.lat}&longitude=${position.lon}&maxradiuskm=${radius}`
-      //);
+      this._quakes = await getJSON(
+      this.baseUrl +
+       `&starttime=2019-01-01&endtime=2019-03-02&latitude=${position.lat}&longitude=${position.lon}&maxradiuskm=${radius}`
+      );
       return this._quakes.features.filter(item => item.id === id)[0];
   }
 }
