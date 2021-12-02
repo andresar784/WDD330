6bf72e16e733309fd5de01f970b014b97f1f3caf
Andres Rodriguez <rod16015@byui.edu>
2021-12-02 15:54:12 -0300

    more functions add

--- finalproject/main.js
+++ finalproject/main.js
@@ -1,12 +1,16 @@
 let flag = document.getElementById('flag');
-let typeOfVehicles =  document.querySelector('#type');
-let vehicle = typeOfVehicles.value;
+
 let brands = document.getElementById('brands');
 //the model select element
 let models = document.getElementById('models'); 
-let modelBtn = document.getElementById('modelBtn')
+let modelBtn = document.getElementById('modelBtn');
+//the year select element
+let years
+let yearBtn = document.getElementById('yearBtn');
 
 flag.addEventListener('click', () => {
+    let typeOfVehicles =  document.querySelector('#type');
+    let vehicle = typeOfVehicles.value;
     let urlbrands = `https://parallelum.com.br/fipe/api/v1/${vehicle}/marcas`;
     console.log(urlbrands);
     fetch(urlbrands)
@@ -28,6 +32,62 @@ flag.addEventListener('click', () => {
      }
 )})
 
-modelBtn.addEventListener('click', () =>
-     let
-)
+modelBtn.addEventListener('click', () => {
+    let typeOfVehicles =  document.querySelector('#type');
+    let brandOfV =  document.querySelector('#brands');
+    let codigo = brandOfV.value
+    let vehicle = typeOfVehicles.value;
+     let urlModels = `https://parallelum.com.br/fipe/api/v1/${vehicle}/marcas/${codigo}/modelos`;
+     console.log(urlModels);
+     fetch(urlModels)
+     .then( Response => {
+        if(Response.ok){
+            return Response;
+        }
+        throw Error(Response.statusText);
+    })
+    .then(response => response.json())
+    .then( mods => {
+        console.log(mods);
+        for (let mod of mods.modelos){
+            var x = document.createElement("OPTION");
+            x.setAttribute("value", mod.nome);
+            var t = document.createTextNode(mod.nome);
+            x.appendChild(t);
+            document.getElementById("models").appendChild(x);
+        }
+    } )
+    }
+    )
+
+ yearBtn.addEventListener('click', () => {
+    let typeOfVehicles =  document.querySelector('#type');
+    let brandOfV =  document.querySelector('#brands');
+    let codigo = brandOfV.value
+    let vehicle = typeOfVehicles.value;
+     let urlModels = `https://parallelum.com.br/fipe/api/v1/${vehicle}/marcas/${codigo}/modelos`;
+     console.log(urlModels);
+     fetch(urlModels)
+     .then( Response => {
+        if(Response.ok){
+            return Response;
+        }
+        throw Error(Response.statusText);
+    })
+    .then(response => response.json())
+    .then( mods => {
+        console.log(mods);
+        for (let mod of mods.anos){
+            var x = document.createElement("OPTION");
+            x.setAttribute("value", mod.nome);
+            var t = document.createTextNode(mod.nome);
+            x.appendChild(t);
+            document.getElementById("year").appendChild(x);
+        }
+    } )
+    }
+    )
+        
+        
+
+  
