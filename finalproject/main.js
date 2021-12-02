eb5069ee3f59a60e3021b498ccf6cefe2293c806
Andres Rodriguez <rod16015@byui.edu>
2021-12-02 12:22:57 -0300

    added more in final project

--- finalproject/main.js
+++ finalproject/main.js
@@ -1,13 +1,14 @@
 let flag = document.getElementById('flag');
+let typeOfVehicles =  document.querySelector('#type');
+let vehicle = typeOfVehicles.value;
 let brands = document.getElementById('brands');
+//the model select element
+let models = document.getElementById('models'); 
+let modelBtn = document.getElementById('modelBtn')
 
 flag.addEventListener('click', () => {
-    
-    let typeOfVehicles =  document.querySelector('#type');
-    let vehicle = typeOfVehicles.value;
-    let urlbrands = 'https://parallelum.com.br/fipe/api/v1/${vehicle}/marcas';
-
-    console.log(vehicle);
+    let urlbrands = `https://parallelum.com.br/fipe/api/v1/${vehicle}/marcas`;
+    console.log(urlbrands);
     fetch(urlbrands)
     .then( Response => {
         if(Response.ok){
@@ -16,5 +17,17 @@ flag.addEventListener('click', () => {
         throw Error(Response.statusText);
     })
     .then(response => response.json())
-    .then( brands  => console.log(brands));
-})
+    .then( marcas  => {
+        for (let marca of marcas) {
+            var x = document.createElement("OPTION");
+            x.setAttribute("value", marca.codigo);
+            var t = document.createTextNode(marca.nome);
+            x.appendChild(t);
+            document.getElementById("brands").appendChild(x);
+        }
+     }
+)})
+
+modelBtn.addEventListener('click', () =>
+     let
+)
