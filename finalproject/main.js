90ef42916d293b80e2b1d9976436f429e1bc759c
Andres Rodriguez <rod16015@byui.edu>
2021-12-04 23:47:23 -0300

    added minor changes

--- finalproject/main.js
+++ finalproject/main.js
@@ -8,7 +8,7 @@ let modelBtn = document.getElementById('modelBtn');
 let years
 let yearBtn = document.getElementById('yearBtn');
 //the last button to consult something
-let lastButton = document.getElementById('lastButton');
+let lastButton = document.getElementById('lastConsult');
 
 flag.addEventListener('click', () => {
     let typeOfVehicles =  document.querySelector('#type');
@@ -53,7 +53,7 @@ modelBtn.addEventListener('click', () => {
         console.log(mods);
         for (let mod of mods.modelos){
             var x = document.createElement("OPTION");
-            x.setAttribute("value", mod.nome);
+            x.setAttribute("value", mod.codigo);
             var t = document.createTextNode(mod.nome);
             x.appendChild(t);
             document.getElementById("models").appendChild(x);
@@ -89,15 +89,25 @@ modelBtn.addEventListener('click', () => {
     } )
     }
     )
+
 //function the get the last result, the price
 lastButton.addEventListener('click', () => {
+    //type of vehicle, cars, motorcycles, trucks
     let typeOfVehicles =  document.querySelector('#type');
+    let vehicle = typeOfVehicles.value;
+    //codigo of the brand selected 
     let brandOfV =  document.querySelector('#brands');
     let codigo = brandOfV.value
-    let vehicle = typeOfVehicles.value;
-    let urlconsult = `https://parallelum.com.br/fipe/api/v1/${vehicle}/marcas/${codigo}/modelos/5940/anos/2014-3`;
-     console.log(urlModels);
-     fetch(urlModels)
+    // year of the vehicle
+    let yearOfV = document.querySelector('#year');
+    let year = yearOfV.value;
+    // I need the number of the model that is returned in year search
+    let modelNumber =  document.querySelector('#models');
+    let modelNum = modelNumber.value;
+    // the last url example is https://parallelum.com.br/fipe/api/v1/carros/marcas/59/modelos/5940/anos/2014-3
+    let urlConsult = `https://parallelum.com.br/fipe/api/v1/${vehicle}/marcas/${codigo}/modelos/${modelNum}/anos/${year}`;
+     console.log('the last consult is ' + urlConsult);
+     fetch(urlConsult)
      .then( Response => {
         if(Response.ok){
             return Response;
@@ -106,17 +116,13 @@ lastButton.addEventListener('click', () => {
     })
     .then(response => response.json())
     .then( mods => {
-        console.log(mods);
-        for (let mod of mods.anos){
-            var x = document.createElement("OPTION");
-            x.setAttribute("value", mod.nome);
-            var t = document.createTextNode(mod.nome);
-            x.appendChild(t);
-            document.getElementById("year").appendChild(x);
+        var x = document.createElement("DIV");
+        x.setAttribute("name", mods.Valor);
+        var t = document.createTextNode(mods.Valor);
+        x.appendChild(t);
+        document.getElementById("infoDiv").appendChild(x);
         }
-    } )
-    }
-    )
+)})
         
 
   
