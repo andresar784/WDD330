d0a5fb2c9f9c6720a4e547dcede59aba403f8651
Andres Rodriguez <rod16015@byui.edu>
2021-12-15 11:42:58 -0300

    table added

--- finalproject/main.js
+++ finalproject/main.js
@@ -118,7 +118,7 @@ lastButton.addEventListener('click', () => {
         console.log(mods)
         var x = document.createElement("DIV");
         x.setAttribute("name", mods.Valor);
-        var t = document.createTextNode(`The value of ` + mods.Marca + ' ' + mods.Modelo + ' year ' + mods.AnoModelo + ' is: ' + mods.Valor);
+        var t = document.createTextNode(`The value of ${mods.Modelo} is: ${mods.Valor}`);
         x.appendChild(t);
         document.getElementById("infoDiv").appendChild(x);
         }
