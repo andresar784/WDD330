8f99c7c0a2f01d4d46257b8b8f4f12357494a3f6
Andres Rodriguez <rod16015@byui.edu>
2021-12-15 11:06:29 -0300

    tables added

--- finalproject/main.js
+++ finalproject/main.js
@@ -6,7 +6,7 @@ let modelBtn = document.getElementById('modelBtn');
 let yearBtn = document.getElementById('yearBtn');
 //the last button to consult something
 let lastButton = document.getElementById('lastConsult');
-
+let moreInfobtn = document.getElementById('moreInfo');
 flag.addEventListener('click', () => {
     let typeOfVehicles =  document.querySelector('#type');
     let vehicle = typeOfVehicles.value;
@@ -106,8 +106,7 @@ lastButton.addEventListener('click', () => {
     let modelNum = modelNumber.value;
     // the last url example is https://parallelum.com.br/fipe/api/v1/carros/marcas/59/modelos/5940/anos/2014-3
     let urlConsult = `https://parallelum.com.br/fipe/api/v1/${vehicle}/marcas/${codigo}/modelos/${modelNum}/anos/${year}`;
-     console.log('the last consult is ' + urlConsult);
-     fetch(urlConsult)
+    fetch(urlConsult)
      .then( Response => {
         if(Response.ok){
             return Response;
@@ -124,6 +123,68 @@ lastButton.addEventListener('click', () => {
         document.getElementById("infoDiv").appendChild(x);
         }
 )})
-        
 
-  
+moreInfobtn.addEventListener('click', () => {
+    let table = document.getElementById('table');
+    //type of vehicle, cars, motorcycles, trucks
+    let typeOfVehicles =  document.querySelector('#type');
+    let vehicle = typeOfVehicles.value;
+    //codigo of the brand selected 
+    let brandOfV =  document.querySelector('#brands');
+    let codigo = brandOfV.value
+    // year of the vehicle
+    let yearOfV = document.querySelector('#year');
+    let year = yearOfV.value;
+    // I need the number of the model that is returned in year search
+    let modelNumber =  document.querySelector('#models');
+    let modelNum = modelNumber.value;
+    // the last url example is https://parallelum.com.br/fipe/api/v1/carros/marcas/59/modelos/5940/anos/2014-3
+    let urlConsult = `https://parallelum.com.br/fipe/api/v1/${vehicle}/marcas/${codigo}/modelos/${modelNum}/anos/${year}`;
+    fetch(urlConsult)
+     .then( Response => {
+        if(Response.ok){
+            return Response;
+        }
+        throw Error(Response.statusText);
+    })
+    .then(response => response.json())
+    .then( mods => {
+        console.table(mods);
+        let innerHtml = '';
+        innerHtml += buildRow(mods);
+        let table = document.getElementById('table1');
+        table.innerHTML = innerHtml;        //  push in our created HTML text
+
+    }
+    )
+})
+
+function buildRow(mods) {
+   return `
+<tr>
+    <th scope="row">Brand <td> ${mods.Marca}</td></th>
+</tr>
+<tr>
+    <th scope="row">Year <td>${mods.AnoModelo}</td></th>
+</tr>
+<tr>
+    <th scope="row">Model <td>${mods.Modelo}</td></th>
+</tr>
+<tr>
+    <th scope="row">Codigo Fipe <td>${mods.CodigoFipe}</td></th>
+</tr>
+<tr>
+    <th scope="row">Value <td>${mods.Valor}</td></th>
+</tr>
+<tr>
+    <th scope="row">Fuel <td>${mods.Combustivel}</td></th>
+</tr>
+<tr>
+    <th scope="row">Reference Month <td>${mods.MesReferencia}</td></th>
+</tr>
+
+
+
+`;
+}
+
